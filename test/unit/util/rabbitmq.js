27484cb21973a78d650da7f8164385491a0786ca
thejsj <jorge.silva@thejsj.com>
2016-08-01 16:40:25 -0700

    Extend RabbitMQClient from ponos

--- test/unit/util/rabbitmq.js
+++ test/unit/util/rabbitmq.js
@@ -34,55 +34,6 @@ describe('RabbitMQ', () => {
     rabbitMQ = new rabbitMQInstance.constructor()
   })
 
-  describe('constructor', () => {
-    it('should have a `_rabbit` property', () => {
-      expect(rabbitMQ._rabbit).to.be.an.object
-      expect(rabbitMQ._rabbit.publishTask).to.be.a.function
-    })
-  })
-
-  describe('connect', () => {
-    it('should connect to the rabbit server', () => {
-      return rabbitMQ.connect()
-        .then(() => {
-          sinon.assert.calledOnce(connectStub)
-        })
-    })
-
-    it('should return an error if `connect` returns an error', done => {
-      let originalErr = new Error('Error connecting')
-      connectStub.rejects(originalErr)
-      rabbitMQ.connect()
-        .asCallback(err => {
-          sinon.assert.calledOnce(connectStub)
-          expect(err).to.exist
-          expect(err).to.equal(originalErr)
-          done()
-        })
-    })
-  })
-
-  describe('disconnect', () => {
-    it('should disconnect from the rabbit server', () => {
-      return rabbitMQ.disconnect()
-        .then(() => {
-          sinon.assert.calledOnce(disconnectStub)
-        })
-    })
-
-    it('should return an error if `disconnect` returns an error', done => {
-      let originalErr = new Error('Error disconnecting')
-      disconnectStub.rejects(originalErr)
-      rabbitMQ.disconnect()
-        .asCallback(err => {
-          sinon.assert.calledOnce(disconnectStub)
-          expect(err).to.exist
-          expect(err).to.equal(originalErr)
-          done()
-        })
-    })
-  })
-
   describe('#publishInvoiceCreated', () => {
     let stripeCustomerId
     let validJob
