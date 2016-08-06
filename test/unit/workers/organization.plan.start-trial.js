baedde831082723eb0da40f6d00d7ea5827d7db0
thejsj <jorge.silva@thejsj.com>
2016-08-05 18:15:58 -0700

    Update client. Fix tests

--- test/unit/workers/organization.plan.start-trial.js
+++ test/unit/workers/organization.plan.start-trial.js
@@ -1,6 +1,12 @@
 'use strict'
 
+const Promise = require('bluebird')
 const expect = require('chai').expect
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
+
+const bigPoppa = require('util/big-poppa')
+const stripe = require('util/stripe')
 
 const WorkerStopError = require('error-cat/errors/worker-stop-error')
 
@@ -10,9 +16,29 @@ describe('#organization.plan.start-trial', () => {
   let validJob
   let tid = '6ab33f93-118a-4a03-bee4-89ddebeab346'
   let organizationId = 67898
+  let getOrganizationStub
+  let updateOrganizationStub
+  let createCustomerStub
 
   beforeEach(() => {
     validJob = { organizationId: organizationId }
+  })
+
+  beforeEach(() => {
+    getOrganizationStub = sinon.stub(bigPoppa, 'getOrganization').resolves({ id: 2 })
+    updateOrganizationStub = sinon.stub(bigPoppa, 'updateOrganization').resolves({})
+    createCustomerStub = sinon.stub(stripe, 'createCustomerAndSubscriptionForOrganization').resolves({
+      customer: {},
+      subscription: {
+        trial_end: '234'
+      }
+    })
+  })
+
+  afterEach(() => {
+    getOrganizationStub.restore()
+    updateOrganizationStub.restore()
+    createCustomerStub.restore()
   })
 
   describe('Validation', () => {
--- test/unit/workers/organization.plan.update.js
+++ test/unit/workers/organization.plan.update.js
@@ -1,6 +1,12 @@
 'use strict'
 
+const Promise = require('bluebird')
 const expect = require('chai').expect
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
+
+const bigPoppa = require('util/big-poppa')
+const stripe = require('util/stripe')
 
 const WorkerStopError = require('error-cat/errors/worker-stop-error')
 
@@ -10,9 +16,24 @@ describe('#organization.plan.update', () => {
   let validJob
   let tid = '6ab33f93-118a-4a03-bee4-89ddebeab346'
   let organizationId = 67898
+  let getOrganizationStub
+  let updateOrganizationStub
+  let updateUsersForPlanStub
 
   beforeEach(() => {
     validJob = { organizationId: organizationId }
+  })
+
+  beforeEach(() => {
+    getOrganizationStub = sinon.stub(bigPoppa, 'getOrganization').resolves({ id: 2, stripeCustomerId: 'cus_234234' })
+    updateOrganizationStub = sinon.stub(bigPoppa, 'updateOrganization').resolves({})
+    updateUsersForPlanStub = sinon.stub(stripe, 'updateUsersForPlan').resolves()
+  })
+
+  afterEach(() => {
+    getOrganizationStub.restore()
+    updateOrganizationStub.restore()
+    updateUsersForPlanStub.restore()
   })
 
   describe('Validation', () => {
