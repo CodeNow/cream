c9dc8caffb1fcc209799f3656292180c12479ab7
thejsj <jorge.silva@thejsj.com>
2016-09-07 12:43:31 -0700

    Add organization.trial.ended.check worker and tests

---
+++ test/integration/workers/organization.trial.ended.check.js
@@ -0,0 +1,147 @@
+'use strict'
+require('loadenv')()
+
+const Promise = require('bluebird')
+const expect = require('chai').expect
+const sinon = require('sinon')
+
+const moment = require('moment')
+const MockAPI = require('mehpi')
+const bigPoppaAPI = new MockAPI('5678')
+
+const OrganizationWithStripeCustomerIdFixture = require('../../fixtures/big-poppa/organization-with-stripe-customer-id')
+
+if (process.env.TEST_STUB_OUT_BIG_POPPA) {
+  process.env.BIG_POPPA_HOST = '127.0.0.1:5678'
+}
+
+const runnableAPI = require('util/runnable-api-client')
+const stripe = require('util/stripe')
+const rabbitmq = require('util/rabbitmq')
+
+const testUtil = require('../../util')
+
+const workerServer = require('workers/server')
+const httpServer = require('http/server')
+
+describe('#organization.trial.ended.check Integration Test', () => {
+  const trialEnd = moment().add(7, 'days')
+  const org1 = Object.assign({}, OrganizationWithStripeCustomerIdFixture, { id: 91, githubId: 9487339, trialEnd })
+  const org2 = Object.assign({}, OrganizationWithStripeCustomerIdFixture, { id: 92, githubId: 10224621, trialEnd })
+  const org3 = Object.assign({}, OrganizationWithStripeCustomerIdFixture, { id: 93, githubId: 2828361, trialEnd })
+  const org3Id = org3.id
+  let org3SubscriptionId
+  let publisher
+
+  let updateSubscriptionWithTrialEndedNotificationStub
+  let publishEventStub
+
+  // HTTP
+  before('Start HTTP server', () => httpServer.start())
+  after('Stop HTTP server', () => httpServer.stop())
+
+  // Runnable API Client
+  before('Login into runnable API', function () {
+    this.timeout(5000)
+    return runnableAPI.login()
+  })
+  after('Logout into runnable API', () => runnableAPI.logout())
+
+  // RabbitMQ Client
+  before('Connect to RabbitMQ', () => rabbitmq.connect())
+  after('Disconnect from RabbitMQ', () => rabbitmq.disconnect())
+
+  // RabbitMQ Publisher
+  before('Connect to RabbitMQ', () => {
+    return testUtil.connectToRabbitMQ(workerServer, ['organization.trial.ended.check'], [])
+      .then(p => { publisher = p })
+  })
+  after('Disconnect from RabbitMQ', () => {
+    return testUtil.disconnectToRabbitMQ(publisher, workerServer)
+  })
+
+  before('Spy on calls', () => {
+    updateSubscriptionWithTrialEndedNotificationStub = sinon.spy(stripe, 'updateSubscriptionWithTrialEndedNotification')
+    publishEventStub = sinon.spy(rabbitmq, 'publishEvent')
+  })
+  after('Restore spies', () => {
+    updateSubscriptionWithTrialEndedNotificationStub.restore()
+    publishEventStub.restore()
+  })
+
+  before('Create customer and subscription', function () {
+    this.timeout(10000)
+    return Promise.all([
+      testUtil.createCustomerAndSubscription(org1),
+      testUtil.createCustomerAndSubscription(org2),
+      testUtil.createCustomerAndSubscription(org3)
+    ])
+      .spread((res1, res2, res3) => {
+        // Save the subscription id for org3
+        org3SubscriptionId = res3.subscription.id
+        // Update org 2 subscription to have correct metadata
+        return stripe.stripeClient.subscriptions.update(res2.subscription.id, {
+          metadata: {
+            notifiedTrialEnded: moment().toISOString()
+          }
+        })
+      })
+  })
+  after('Clean up Stripe', function () {
+    this.timeout(5000)
+    // Deleting the customer deletes the subscription
+    return Promise.all([
+      stripe.stripeClient.customers.del(org1.stripeCustomerId),
+      stripe.stripeClient.customers.del(org2.stripeCustomerId),
+      stripe.stripeClient.customers.del(org3.stripeCustomerId)
+    ])
+  })
+
+  // Big Poppa Mock
+  before('Stub out big-poppa calls', done => {
+    // Set hasPaymentMethod to true and false
+    org1.hasPaymentMethod = false
+    org2.hasPaymentMethod = false
+    org3.hasPaymentMethod = false
+    // Change trielEndDate on org 3 and 4 to be ended organization
+    org2.trialEnd = moment().subtract(1, 'days')
+    org3.trialEnd = moment().subtract(1, 'hours')
+    bigPoppaAPI.stub('GET', '/organization/?hasPaymentMethod=false').returns({
+      status: 200,
+      body: JSON.stringify([org1, org2, org3])
+    })
+    bigPoppaAPI.start(done)
+  })
+  after(done => {
+    bigPoppaAPI.restore()
+    bigPoppaAPI.stop(done)
+  })
+
+  it('should publish the `organiztion.trial.ended` event', function () {
+    this.timeout(5000)
+    publisher.publishTask('organization.trial.ended.check', {})
+
+    const checkCustomerCreated = Promise.method(() => {
+      // Check if spy has been called
+      return !!updateSubscriptionWithTrialEndedNotificationStub.called
+    })
+    return testUtil.poll(checkCustomerCreated, 100, 5000)
+      .delay(1000)
+      .then(function checkStripeForUpdatePlan () {
+        // Assert the event was published
+        sinon.assert.calledOnce(publishEventStub)
+        sinon.assert.calledWith(publishEventStub, 'organization.trial.ended', {
+          organization: {
+            id: org3Id,
+            name: org3.name
+          },
+          tid: sinon.match.string
+        })
+        // Assert Stripe was updated
+        return stripe.stripeClient.subscriptions.retrieve(org3SubscriptionId)
+          .then(subscription => {
+            expect(subscription).to.have.deep.property('metadata.notifiedTrialEnded')
+          })
+      })
+  })
+})
--- test/unit/workers/organization.trial.ended.check.js
+++ test/unit/workers/organization.trial.ended.check.js
@@ -1,35 +1,166 @@
 'use strict'
 
+const Promise = require('bluebird')
+const Joi = Promise.promisifyAll(require('joi'))
+const sinon = require('sinon')
+require('sinon-as-promised')(Promise)
 const expect = require('chai').expect
+const moment = require('moment')
 
-const WorkerStopError = require('error-cat/errors/worker-stop-error')
+const testUtil = require('../../util')
 
-const CheckForOrganizationsWithEndedTrials = require('workers/organization.trial.ended.check')
+const stripe = require('util/stripe')
+const TrialService = require('services/trial-service')
+const rabbitmq = require('util/rabbitmq')
+
+const CheckForOrganizationsWithEndedTrials = require('workers/organization.trial.ended.check').task
+const CheckForOrganizationsWithEndedTrialsSchema = require('workers/organization.trial.ended.check').jobSchema
 
 describe('#organization.trial.ended.check', () => {
   let validJob
+  let getFilteredOrgsInTrialByTrialEndTimeStub
+  let publishEventStub
+  let updateSubscriptionWithTrialEndedNotificationStub
+  let filterSpy
+  const org3Id = 3
+  const org3Name = 'HelloWorld'
+  const org1 = {
+    id: 1,
+    name: 'org1',
+    subscription: {
+      metadata: {
+        notifiedTrialEnded: moment().toISOString()
+      }
+    }
+  }
+  const org2 = {
+    id: 2,
+    name: 'org2'
+  }
+  const org3 = {
+    id: org3Id,
+    name: org3Name,
+    subscription: {
+      id: 'sub_2342323'
+    }
+  }
 
-  beforeEach(() => {
+  beforeEach('Set valid job', () => {
     validJob = {}
+  })
+
+  beforeEach('Stub out methods', () => {
+    getFilteredOrgsInTrialByTrialEndTimeStub = sinon.stub(TrialService, 'getFilteredOrgsInTrialByTrialEndTime').resolves([org1, org2, org3])
+    publishEventStub = sinon.stub(rabbitmq, 'publishEvent')
+    updateSubscriptionWithTrialEndedNotificationStub = sinon.stub(stripe, 'updateSubscriptionWithTrialEndedNotification').resolves()
+    filterSpy = sinon.spy(Promise, 'filter')
+  })
+  afterEach('Retore methods', () => {
+    getFilteredOrgsInTrialByTrialEndTimeStub.restore()
+    publishEventStub.restore()
+    updateSubscriptionWithTrialEndedNotificationStub.restore()
+    filterSpy.restore()
   })
 
   describe('Validation', () => {
     it('should validate if a valid job is passed', () => {
-      return CheckForOrganizationsWithEndedTrials(validJob)
+      return Joi.validateAsync(validJob, CheckForOrganizationsWithEndedTrialsSchema)
     })
 
     it('should not validate if `tid` is not a uuid', done => {
-      CheckForOrganizationsWithEndedTrials({ tid: 'world' })
+      return Joi.validateAsync({ tid: 'world' }, CheckForOrganizationsWithEndedTrialsSchema)
         .asCallback(err => {
           expect(err).to.exist
-          expect(err).to.be.an.instanceof(WorkerStopError)
-          expect(err.message).to.match(/invalid.*job/i)
+          expect(err.message).to.match(/tid.*guid/i)
           done()
         })
     })
   })
 
-  xdescribe('Errors', () => {})
+  describe('Errors', () => {
+    it('should throw an error if an organization cannot be updated', () => {
+      let thrownErr = new Error('hello')
+      getFilteredOrgsInTrialByTrialEndTimeStub.rejects(thrownErr)
 
-  xdescribe('Main Functionality', () => {})
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(testUtil.throwIfSuccess)
+      .catch(err => {
+        expect(err).to.exist
+        expect(err).to.equal(thrownErr)
+      })
+    })
+
+    it('should throw an error if it cant update the subscription', () => {
+      let thrownErr = new Error('hello')
+      const orgThatWillFail = Object.assign({}, org2, { 'subscription': { id: 'sub_23423888900' } })
+      getFilteredOrgsInTrialByTrialEndTimeStub.resolves([ org1, orgThatWillFail, org3 ])
+      updateSubscriptionWithTrialEndedNotificationStub.onCall(0).rejects(thrownErr)
+
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(testUtil.throwIfSuccess)
+      .catch(err => {
+        expect(err).to.exist
+        expect(err).to.equal(thrownErr)
+      })
+    })
+  })
+
+  describe('Main Functionality', () => {
+    it('should call `getFilteredOrgsInTrialByTrialEndTime`', () => {
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(() => {
+        sinon.assert.calledOnce(getFilteredOrgsInTrialByTrialEndTimeStub)
+        sinon.assert.calledWithExactly(
+          getFilteredOrgsInTrialByTrialEndTimeStub,
+          sinon.match.string
+        )
+      })
+    })
+
+    it('should filter out orgs with `notifiedTrialEnded`', () => {
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(() => {
+        sinon.assert.calledOnce(filterSpy)
+        sinon.assert.calledWithExactly(filterSpy, [org2, org3], sinon.match.func)
+      })
+    })
+
+    it('should update the subscription with the `notifiedTrialEnded` property for orgs with a subscription', () => {
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(() => {
+        sinon.assert.calledOnce(updateSubscriptionWithTrialEndedNotificationStub)
+        sinon.assert.calledWithExactly(
+          updateSubscriptionWithTrialEndedNotificationStub,
+          org3.subscription.id,
+          sinon.match.string
+        )
+      })
+    })
+
+    it('should publish a `trial.expiring` event for all unotified orgs', () => {
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(() => {
+        sinon.assert.calledOnce(publishEventStub)
+        sinon.assert.calledWithExactly(
+          publishEventStub,
+          'organization.trial.ended',
+          {
+            organization: {
+              id: org3Id,
+              name: org3Name
+            }
+          }
+        )
+      })
+    })
+
+    it('should return an array of org ids', () => {
+      return CheckForOrganizationsWithEndedTrials(validJob)
+      .then(orgIds => {
+        expect(orgIds).to.be.an('array')
+        expect(orgIds).to.have.lengthOf(1)
+        expect(orgIds[0]).to.equal(org3Id)
+      })
+    })
+  })
 })
