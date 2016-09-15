fad5d2a329a5f6b2126105bdf6dfb3dbb84b4bf1
thejsj <jorge.silva@thejsj.com>
2016-09-15 11:26:11 -0700

    Fix stripe.invoice.created worker

--- test/unit/workers/stripe.invoice.created.js
+++ test/unit/workers/stripe.invoice.created.js
@@ -48,7 +48,7 @@ describe('#stripe.invoice.created', () => {
   beforeEach('Stub out', () => {
     getOrganizationsStub = sinon.stub(bigPoppa, 'getOrganizations').resolves(OrganizationsFixture)
     updatePlanIdForOrganizationBasedOnCurrentUsageStub = sinon.stub(stripe, 'updatePlanIdForOrganizationBasedOnCurrentUsage').resolves()
-    updateInvoiceWithPaymentMethodOwnerStub = sinon.stub(stripe.invoice, 'updateWithPaymentMethodOwner').resolves()
+    updateInvoiceWithPaymentMethodOwnerStub = sinon.stub(stripe.invoices, 'updateWithPaymentMethodOwner').resolves()
     getEventStub = sinon.stub(stripe, 'getEvent').resolves(stripeEvent)
   })
   afterEach(() => {
