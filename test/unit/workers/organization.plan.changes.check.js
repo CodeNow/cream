6398e4ee047592ce879b72d85f0cd5501567152a
Anton Podviaznikov <podviaznikov@gmail.com>
2016-10-04 16:43:30 -0700

    delete unused code

--- test/unit/workers/organization.plan.changes.check.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-describe('#organization.plan.changes-check', () => {
-  xdescribe('Validation', () => {})
-
-  xdescribe('Errors', () => {})
-
-  xdescribe('Main Functionality', () => {})
-})
