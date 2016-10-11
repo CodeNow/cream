e9bc703b563f4905f1ab753ef1f7005bbfb8caac
thejsj <jorge.silva@thejsj.com>
2016-10-10 18:47:17 -0700

    Add stripe validation checking

--- test/unit/workers/error-handler.js
+++ test/unit/workers/error-handler.js
@@ -4,6 +4,7 @@ const expect = require('chai').expect
 const Joi = require('util/joi')
 
 const EntityNotFoundError = require('errors/entity-not-found-error')
+const ValidationError = require('errors/validation-error')
 const WorkerStopError = require('error-cat/errors/worker-stop-error')
 
 const errorHandler = require('workers/error-handler')
@@ -23,6 +24,14 @@ describe('error-handler', () => {
         errorHandler(err)
       }).to.throw(WorkerStopError)
         .and.to.match(/invalid.*job/i)
+    })
+
+    it('should throw a `WorkerStopError` when a job is not validated', () => {
+      const err = new ValidationError()
+      expect(() => {
+        errorHandler(err)
+      }).to.throw(WorkerStopError)
+        .and.to.match(/validation.*error/i)
     })
 
     it('should re-throw an unmatched error', () => {
